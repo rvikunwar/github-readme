7f41609e1374d26abef1a1993b536f1950814c98
Ravi Kunwar <ravikunwar079@gmail.com>
2023-09-20 18:24:09 +0530

    fix: layout

--- src/components/preview/index.tsx
+++ src/components/preview/index.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
-import rehypeRaw from "rehype-raw";
+import rehypeHighlight from 'rehype-highlight'
+import rehypeRaw from 'rehype-raw'
+import remarkGfm from 'remark-gfm'
+import remarkSlug from 'remark-slug'
+import remarkToc from 'remark-toc'
 
 function MarkdownPreview({
   markdownValue,
 }: {
   markdownValue: string | undefined;
 }) {
+
+  /** @type {import('unified').PluggableList} */
+  const rehypePlugins = [rehypeHighlight, {ignoreMissing: true}, rehypeRaw as any]
+  /** @type {import('unified').PluggableList} */
+  const remarkPlugins = [remarkSlug, remarkToc, remarkGfm]
+
   return (
     <ReactMarkdown
-      className="min-h-screen"
-      rehypePlugins={[rehypeRaw as any]}
-      remarkPlugins={[remarkGfm]}
+      rehypePlugins={rehypePlugins}
+      remarkPlugins={remarkPlugins}
     >
       {markdownValue ? markdownValue : ""}
     </ReactMarkdown>
